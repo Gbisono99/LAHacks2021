ca77fb50b5ac73a48c11cd73f417896426b39800
Gbisono99 <geovanni.bisono.396@my.csun.edu>
2021-03-23 17:31:37 -0700

    Created server_info (IC)

--- information.py
+++ information.py
@@ -1,11 +1,25 @@
 class Event:
-    def __init__(self,event_name,event_description,points):
-        self.event_name = event_name #The events name
+    def __init__(self,name,event_description,points):
+        self.name = name #The events name
         self.event_description = event_description #The events description
         self.points = points #The amount of points that are given per completion of said event.
 class Person:
-    def __init__(self,member_id,level,roles,date):
-        self.member_id = member_id #Member's name
+    def __init__(self,name,level,roles,date):
+        self.name = name #Member's name
         self.level = level #Member's level
         self.roles = roles #this is a list of Role objects; A list of the Member's current role
         self.date = date #Member's joined date
+class ServerInfomation:
+    def __init__(self,name,member_list,roles_list,text_list,voice_list,curr_event_list):
+        #Member List
+        #Roles
+        #Text Channels
+        #Voice Channels
+        #Current Events
+        self.name = name
+        self.member_list = member_list
+        self.roles_list = roles_list
+        self.text_list = text_list
+        self.voice_list = voice_list
+        self.curr_event_list = curr_event_list
+
--- mainbot.py
+++ mainbot.py
@@ -8,23 +8,27 @@ from discord.ext.commands import Context
 import asyncio
 from information import Event
 from information import Person
+from information import ServerInfomation
 
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix = '$', intents = intents)
+client = commands.Bot(command_prefix = '\\', intents = intents)
 token = 'ODI0MDM3OTc5MTMwNjkxNjI0.YFpjLA.0fkY4p7btGEDgaetEfHNVosI0B0'
 events = {} #Key = event name,  value = Event object from information.py
 all_people = [] #A list of Person objects from information.py. Basically each member's information.
 
 guild_id = 824048886746185758 #Guild's current id.
-server = None #Initalized to None but on_ready() will set it to the given server.
+guild_global = None #Initalized to None but on_ready() will set it to the given server.
 #A list of Roles might be useful?
+server_information = [] #Server information
 
 #Discord Events, not line 9.
 @client.event
 async def on_ready():
     print('Bot is online.')
-    server = client.get_guild(guild_id) #Server information
+    guild_global = client.get_guild(guild_id) #Server information
+    server_information.append(ServerInfomation(guild_global.name,guild_global.members,guild_global.roles,guild_global.text_channels,guild_global.voice_channels,events))
+    print(server_information[0].member_list)
 @client.event
 async def on_member_join(member):
     pass
@@ -32,7 +36,16 @@ async def on_member_join(member):
 #@Admins and @Event Organizers
 @client.command()
 async def server_info(ctx):
-    pass
+    temp = serverformat(server_information[0].member_list)
+    #info_members = f'Members: \n {temp} \n'
+    #info_roles = 'Roles: \n' + serverformat(server_information.roles_list) + '\n'
+    #info_texts = 'Texts: \n' + serverformat(server_information.texts_list) + '\n'
+    #info_voices = 'Voices: \n' + serverformat(server_information.voice_list) + '\n'
+    #info_events = 'Current Events: \n' + serverformat(server_information.curr_event_list) + '\n'
+
+    #description = f'{info_members}+{info_roles}+{info_texts}+{info_voices}+{info_events}'
+    #embed = discord.Embed(title = f'{server_information.name}\'s Server Information', description = description)
+    #await ctx.send(embed = embed)
 @client.command()
 async def event_create(ctx):
     pass
@@ -42,9 +55,9 @@ async def event_delete(ctx):
 @client.command()
 async def points_for_role(ctx):
     pass
-@client.command()
-async def update_json(ctx):
-    pass
+#@client.command()
+#async def update_json(ctx):
+#    pass
 @client.command()
 async def assign_for_points(ctx):
     pass
@@ -61,5 +74,17 @@ async def leave_event(ctx,event):
 @client.command()
 async def event_information(ctx,event):
     pass
-
+@client.command()
+async def all_events(ctx):
+    pass
 client.run(token)
+
+#Standard Methods
+def serverformat(generic_list):
+    description = ''
+    #if generic_list != None:
+    for generic in generic_list:
+        description = description + f'{generic.name}, '
+    return description     
+    #else:
+        #return f'No given information available.'
