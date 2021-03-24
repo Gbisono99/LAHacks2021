c3fbe2f5d0be80bb9ff57ffede2d0a8d69f089bc
Elvis <elvischino754@gmail.com>
2021-03-24 05:15:03 -0700

    Updated event_create

--- mainbot.py
+++ mainbot.py
@@ -40,18 +40,33 @@ async def server_info(ctx):
 # $events_create {event_name} {role_color} {event_description} {points}
 @client.command()
 async def event_create(ctx, *args):
-    role_perm = discord.Permissions(view_channel=False)
-    role = await ctx.guild.create_role(name=args[0], colour=discord.Colour(int(args[1], 16)), permissions=role_perm)
-    event_category = await ctx.guild.create_category(name=args[0])
-    await event_category.create_text_channel(name=args[0])
-    await event_category.create_voice_channel(name=args[0])
-    await event_category.set_permissions(role, read_messages=True, send_messages=True, connect=True, speak=True)
-    await event_category.set_permissions(ctx.guild.default_role, read_messages=False, connect=False)
-    events.append(Event(args[0], args[2], int(args[3], 10), None, role.created_at))
+    if(discord.utils.get(ctx.guild.roles, name=args[0]) is None):
+        if(len(args) == 4):
+            role_perm = discord.Permissions(view_channel=False)
+            role = await ctx.guild.create_role(name=args[0], colour=discord.Colour(int(args[1], 16)), permissions=role_perm)
+            event_category = await ctx.guild.create_category(name=args[0])
+            await event_category.create_text_channel(name=args[0])
+            await event_category.create_voice_channel(name=args[0])
+            await event_category.set_permissions(role, read_messages=True, send_messages=True, connect=True, speak=True)
+            await event_category.set_permissions(ctx.guild.default_role, read_messages=False, connect=False)
+            events.append(Event(args[0], args[2], int(args[3], 10), None, role.created_at))
+        elif(len(args) == 3):
+            role_perm = discord.Permissions(view_channel=False)
+            role = await ctx.guild.create_role(name=args[0], colour=discord.Colour(random.randint(0, 255)), permissions=role_perm)
+            event_category = await ctx.guild.create_category(name=args[0])
+            await event_category.create_text_channel(name=args[0])
+            await event_category.create_voice_channel(name=args[0])
+            await event_category.set_permissions(role, read_messages=True, send_messages=True, connect=True, speak=True)
+            await event_category.set_permissions(ctx.guild.default_role, read_messages=False, connect=False)
+            events.append(Event(args[0], args[1], int(args[2], 10), None, role.created_at))
+    else:
+        await ctx.send(f'{args[0]} Already Exists')
 
 @client.command()
-async def event_delete(ctx):
-    pass
+async def event_delete(ctx, arg):
+    await discord.utils.get(ctx.guild.channels, name=arg).delete()
+    await discord.utils.get(ctx.guild.categories, name=arg).delete()
+    await discord.utils.get(ctx.guild.roles, name=arg).delete()
 
 @client.command()
 async def points_for_role(ctx):
